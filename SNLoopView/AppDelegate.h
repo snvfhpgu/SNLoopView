a219ad992cef155b2008c858832f73a0fcf4dd22
smallpos <smallpos@Mac-mini.local>
2017-11-09 16:12:28 +0800

    Initial Commit

---
+++ SNLoopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNLoopView
+//
+//  Created by smallpos on 2017/11/9.
+//  Copyright © 2017年 snde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
